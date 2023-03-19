30b3075f6eac4b43de93acf48bfd14e051cff8c9
Юрий <124659396+j41kkk@users.noreply.github.com>
2023-03-19 09:38:11 +0300

    Add files via upload

---
+++ appointments/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, reverse, redirect
+from django.views import View
+from django.core.mail import EmailMultiAlternatives  # импортируем класс для создание объекта письма с html
+from datetime import datetime
+from django.template.loader import render_to_string  # импортируем функцию, которая срендерит наш html в текст
+from django.db.models.signals import post_save
+from django.core.mail import mail_managers
+from .models import Appointment
+
+# создаём функцию-обработчик с параметрами под регистрацию сигнала
+def notify_managers_appointment(sender, instance, created, **kwargs):
+    # в зависимости от того, есть ли такой объект уже в базе данных или нет, тема письма будет разная
+    if created:
+        subject = f'{instance.client_name} {instance.date.strftime("%d %m %Y")}'
+    else:
+        subject = f'Appointment changed for {instance.client_name} {instance.date.strftime("%d %m %Y")}'
+
+    mail_managers(
+        subject=subject,
+        message=instance.message,
+    )
+
+# коннектим наш сигнал к функции обработчику и указываем, к какой именно модели после сохранения привязать функцию
+post_save.connect(notify_managers_appointment, sender=Appointment)
+
+class AppointmentView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'make_appointment.html', {})
+
+    def post(self, request, *args, **kwargs):
+        appointment = Appointment(
+            date=datetime.strptime(request.POST['date'], '%Y-%m-%d'),
+            client_name=request.POST['client_name'],
+            message=request.POST['message'],
+        )
+        appointment.save()
+
+        # получаем наш html
+        html_content = render_to_string(
+            'appointment_created.html',
+            {
+                'appointment': appointment,
+            }
+        )
+
+        # в конструкторе уже знакомые нам параметры, да? Называются правда немного по-другому, но суть та же.
+        msg = EmailMultiAlternatives(
+            subject=f'{appointment.client_name} {appointment.date.strftime("%Y-%M-%d")}',
+            body=appointment.message,  # это то же, что и message
+            from_email='peterbadson@yandex.ru',
+            to=['skavik46111@gmail.com'],  # это то же, что и recipients_list
+        )
+        msg.attach_alternative(html_content, "text/html")  # добавляем html
+
+        msg.send()  # отсылаем
+
+        return redirect('appointments:make_appointment')
