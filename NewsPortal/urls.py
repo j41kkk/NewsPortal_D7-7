30ca5aa7050f39bdf14291aa9ea15a19307a90e2
Юрий <124659396+j41kkk@users.noreply.github.com>
2023-03-19 09:37:42 +0300

    Update urls.py

--- NewsPortal/urls.py
+++ NewsPortal/urls.py
@@ -12,9 +12,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
 
 
 urlpatterns = [
